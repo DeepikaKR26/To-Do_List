b088a7b2225115ec289b89291946884026372efd
PALAVALLI S ADITHYA <141756547+Adithyaspalavalli@users.noreply.github.com>
2025-07-20 19:25:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ToDoList from "./assets/ToDoList";
+import React,{ useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState();
+
+  return (
+
+         <ToDoList />
+    
+  )
+}
+
+export default App;
